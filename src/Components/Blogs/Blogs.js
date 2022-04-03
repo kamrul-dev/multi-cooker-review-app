ee22d74df98378e172045735e3be46b91932828e
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-03 22:33:24 +0600

    Component stucuture created

---
+++ src/Components/Blogs/Blogs.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Blogs.css';
+
+const Blogs = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/Components/Reviews/Reviews.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Reviews.css';
+
+const Reviews = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+export default Reviews;
