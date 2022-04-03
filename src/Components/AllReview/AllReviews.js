3a06f5c60b8b2a16c79e88f913385cebe0dc2c8f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-04 04:19:32 +0600

    AllReviews items diplay on UI

--- src/Components/AllReview/AllReviews.js
+++ src/Components/AllReview/AllReviews.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import useReviews from '../../hooks/useReviews';
 
 const AllReviews = () => {
+    const [reviews, setReviews] = useReviews();
     return (
-        <div>
-            <h2>All reviews page</h2>
-        </div>
+        <>
+            <div className='row container mx-auto mt-5 mb-5'>
+                {
+                    reviews.map(review =>
+                        <div className='col-md-4'>
+                            <div className='card d-flex align-items-center reviews-section border-0'>
+                                <img src={review.image} alt="" />
+                                <span className='mt-3'>{review.ratings} star</span>
+                                <div className='card-body'>
+                                    <h5 className='text-center'>{review.name}</h5>
+                                    <p>{review.post}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
+        </>
     );
 };
 
