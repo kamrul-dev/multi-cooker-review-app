9209ff27ed336ac2e79edf1ae9d694de6c5b68c4
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-03 23:24:09 +0600

    Navbar added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -4,13 +4,15 @@ import './Navbar.css';
 
 const Navbar = () => {
     return (
-        <nav className=''>
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/home'></NavLink>
-            <NavLink to='/reviews'>Reviews</NavLink>
-            <NavLink to='/dashboard'>Dashboard</NavLink>
-            <NavLink to='/blogs'>Blogs</NavLink>
-            <NavLink to='/about'>About</NavLink>
+        <nav className='navbar-item'>
+            <div className='container'>
+                <NavLink to='/' className='me-1 p-2 text-decoration-none fs-5 text-white'>Home</NavLink>
+                <NavLink to='/home'></NavLink>
+                <NavLink to='/reviews' className='me-1 p-2 text-decoration-none fs-5 text-white'>Reviews</NavLink>
+                <NavLink to='/dashboard' className='me-1 p-2 text-decoration-none fs-5 text-white'>Dashboard</NavLink>
+                <NavLink to='/blogs' className='me-1 p-2 text-decoration-none fs-5 text-white'>Blogs</NavLink>
+                <NavLink to='/about' className='me-1 p-2 text-decoration-none fs-5 text-white'>About</NavLink>
+            </div>
         </nav>
     );
 };
