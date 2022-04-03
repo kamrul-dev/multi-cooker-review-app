970ddb491223f2830ddb837df3e35389f502ce3f
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-04 03:32:01 +0600

    All reviews button added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -7,8 +7,8 @@ import './Home.css';
 const Home = () => {
 
     const [reviews, setReviews] = useReviews();
- 
-    const cutomerReviews = reviews.length > 3 ? reviews.slice(0, 3): reviews;
+
+    const cutomerReviews = reviews.length > 3 ? reviews.slice(0, 3) : reviews;
     // if(reviews.length > 3){
     //    const customerReviews = reviews.slice(0, 3)
     //     console.log(customerReviews)
@@ -16,8 +16,8 @@ const Home = () => {
     return (
         <>
             <Banner></Banner>
-            <div className='container mx-auto'>
-                <h2 className='text-center mt-4 mb-5' style={{color:'#32a6a8'}}>Reviews By Our Customer</h2>
+            <div className='container mx-auto mb-5'>
+                <h2 className='text-center mt-4 mb-5' style={{ color: '#32a6a8' }}>Reviews By Our Customer</h2>
                 <div className='row'>
                     {
                         cutomerReviews.map(cutomerReview => <Reviews
@@ -25,6 +25,9 @@ const Home = () => {
                             cutomerReview={cutomerReview}
                         ></Reviews>)
                     }
+                </div>
+                <div className='text-center'>
+                    <button className='btn button-in-banner'>See All Reviews</button>
                 </div>
             </div>
         </>
