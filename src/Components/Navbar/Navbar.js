12dad31604cdf450eb98ffe45518d03e1e6cd944
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-04 19:28:06 +0600

    Active NavLink added

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -6,12 +6,12 @@ const Navbar = () => {
     return (
         <nav className='navbar-item'>
             <div className='container'>
-                <NavLink to='/' className='me-1 p-2 text-decoration-none fs-5 text-white'>Home</NavLink>
+                <NavLink to='/' className={({isActive}) => (isActive? 'me-1 p-2 text-decoration-none fs-5 navbar-items-active-color' : 'me-1 p-2 text-decoration-none fs-5 navbar-items-color')}>Home</NavLink>
                 <NavLink to='/home'></NavLink>
-                <NavLink to='/all-reviews' className='me-1 p-2 text-decoration-none fs-5 text-white'>Reviews</NavLink>
-                <NavLink to='/dashboard' className='me-1 p-2 text-decoration-none fs-5 text-white'>Dashboard</NavLink>
-                <NavLink to='/blogs' className='me-1 p-2 text-decoration-none fs-5 text-white'>Blogs</NavLink>
-                <NavLink to='/about' className='me-1 p-2 text-decoration-none fs-5 text-white'>About</NavLink>
+                <NavLink to='/all-reviews' className={({isActive}) => (isActive? 'me-1 p-2 text-decoration-none fs-5 navbar-items-active-color' : 'me-1 p-2 text-decoration-none fs-5 navbar-items-color')}>Reviews</NavLink>
+                <NavLink to='/dashboard' className={({isActive}) => (isActive? 'me-1 p-2 text-decoration-none fs-5 navbar-items-active-color' : 'me-1 p-2 text-decoration-none fs-5 navbar-items-color')}>Dashboard</NavLink>
+                <NavLink to='/blogs' className={({isActive}) => (isActive? 'me-1 p-2 text-decoration-none fs-5 navbar-items-active-color' : 'me-1 p-2 text-decoration-none fs-5 navbar-items-color')}>Blogs</NavLink>
+                <NavLink to='/about' className={({isActive}) => (isActive? 'me-1 p-2 text-decoration-none fs-5 navbar-items-active-color' : 'me-1 p-2 text-decoration-none fs-5 navbar-items-color')}>About</NavLink>
             </div>
         </nav>
     );
