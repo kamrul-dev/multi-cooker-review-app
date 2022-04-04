0594f17727336ba1ab72a3918a1e33e0fae1c65d
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-04 15:02:46 +0600

    Display Investment vs revenue

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import { Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 import useChart from '../../hooks/useChart';
 
 const Dashboard = () => {
@@ -8,14 +8,36 @@ const Dashboard = () => {
         <>
             <div className='row container mx-auto'>
                 <div className='col-md-6'>
-                    <h2>MONTH WISE SELL</h2>
-                    <LineChart width={800} height={500} data={chartDatas}>
+                    <h4 className='text-center mt-5 mb-4'>MONTH WISE SELL</h4>
+                    <LineChart width={500} height={300} data={chartDatas}>
                         <Line dataKey={'price'}></Line>
                         <Line dataKey={'sell'}></Line>
                         <XAxis dataKey={'month'}></XAxis>
                         <YAxis></YAxis>
                         <Tooltip></Tooltip>
                     </LineChart>
+                </div>
+                <div className='col-md-6'>
+                    <h4 className='text-center mt-5 mb-4'>INVESTMENT VS REVENUE</h4>
+                    <AreaChart width={500} height={300} data={chartDatas}
+                        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+                        <defs>
+                            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                            </linearGradient>
+                            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+                                <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+                            </linearGradient>
+                        </defs>
+                        <XAxis dataKey="month" />
+                        <YAxis dataKey="investment" />
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <Tooltip />
+                        <Area type="monotone" dataKey="investment" stroke="#8884d8" fillOpacity={1} fill="url(#colorUv)" />
+                        <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
+                    </AreaChart>
                 </div>
             </div>
         </>
