e8b474196f475368683a6cc3c7dcac9712325ef6
Kamrul Hasan <kamrulsdev@gmail.com>
2022-04-04 19:40:24 +0600

    updated chart color

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -23,8 +23,8 @@ const Dashboard = () => {
                         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                         <defs>
                             <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                                <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+                                <stop offset="5%" stopColor="#1DACBF" stopOpacity={0.8} />
+                                <stop offset="95%" stopColor="#1DACBF" stopOpacity={0} />
                             </linearGradient>
                             <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
                                 <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
